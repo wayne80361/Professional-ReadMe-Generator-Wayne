48f475189e2ba6666952b3e8c0d785bbfd57a0cb
wayne80361 <wayne80361@gmail.com>
2023-09-19 23:25:10 -0400

    add question set

--- index.js
+++ index.js
@@ -6,7 +6,55 @@ const { askLicense } = require("./generateMarkdown");
 // const { chooseLicense } = require("./generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of the project?",
+    name: "project-title",
+  },
+  {
+    type: "input",
+    message: "Description - what was your motivation?",
+    name: "motivation",
+  },
+  {
+    type: "input",
+    message: "Description - why did you build this project?",
+    name: "why-build",
+  },
+  {
+    type: "input",
+    message: "Description - What problem does it solve?",
+    name: "problem-solve",
+  },
+  {
+    type: "input",
+    message: "Description - What did you learn?",
+    name: "learn",
+  },
+  {
+    type: "input",
+    message:
+      "Installation - What are the steps required to install the project?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Usage - Please provide instructions and examples for use",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message:
+      "Contributing - List your collaborators, if any, with links to their GitHub profiles",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Test Instruction",
+    name: "test",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
